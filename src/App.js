6ae4e3953a26bc480cde12026d6c41ebcc830e08
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-12-09 15:01:51 -0800

    adding react routing for soon-to-be nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import React, { Fragment } from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './styles/App.css';
+import Header from './components/common/header';
+import Landing from './components/pages/landing';
+
+
+
+
+function Portfolio() {
+  return (
+    <h1>Portfolio</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About</h1>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <p>testing github</p>
-      <div>
-        <h1>This is an awesome test app</h1>
-      </div>
-    </div>
+    <>  
+        <Header />
+        <Routes>
+          <Route path="/" element={<Landing />}/>
+          <Route path="/portfolio" element={<Portfolio />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>    
+    </>
   );
 }
 
