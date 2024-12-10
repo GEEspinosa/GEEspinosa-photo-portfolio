e58b49f468ccfabfe2dc6d53dc9beb884be3f12a
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-12-10 00:05:36 -0800

    Improving head styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import {Routes, Route} from 'react-router-dom';
 import './styles/App.css';
-import Header from './components/common/header';
+import Header from './components/common/header/header';
 import Landing from './components/pages/landing';
 import Portfolio from './components/pages/portfolio'
 import About from './components/pages/about'
