08303cf72915f6b4fe855b6c2d2e3a5870079315
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-12-14 00:03:01 -0800

    added hamburger icon animation

--- src/components/common/open-menu/styled.open-menu.js
+++ src/components/common/open-menu/styled.open-menu.js
@@ -1,38 +1,32 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 
-const StyledOpenMenu = styled.nav`
-
-        
-        background: gray;
-        overflow-x: hidden;
-        height: 100vh;
-        width: 100%;
-        position: fixed;
-        top: 0;
-        right: 0;
-        transition: transform 0.3s ease-in-out;
-        
-        transform: ${({open}) => open ? 'translateX(0)' : 'translateX(100%)'};
+const StyledOpenMenu = styled.nav`    
+    background: gray;
+    overflow-x: hidden;
+    height: 100vh;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    right: 0;
+    transition: transform 0.3s ease-in-out;   
+    transform: ${({open}) => open ? 'translateX(0)' : 'translateX(100%)'};
 
         div {
-        display: flex;
-        flex-direction: column;
-        //border: solid white;
-        margin-top: 6rem;
+            display: flex;
+            flex-direction: column;
+            margin-top: 6rem;
         }
 `;
 
-const StyledOpenMenuLink  = styled (Link)`
-   
+const StyledOpenMenuLink  = styled (Link)` 
     font-size: 1.5rem;
     text-transform: uppercase;
     padding: 2rem 0;
     font-weight: bold;
     letter-spacing: 0.5rem;
     color: white;
-    text-align: center;
-   
+    text-align: center; 
     text-decoration: none;
     transition: color 0.3s linear;
 
