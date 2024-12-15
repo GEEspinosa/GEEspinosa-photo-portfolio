c378881f6a5fd703123781adc0348b1c96d3a846
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-12-15 02:32:12 -0800

    finished loading and structuring image data array for mapping

--- src/assets/image-data.js
+++ src/assets/image-data.js
@@ -13,6 +13,29 @@ import dannyBassFeedback from './images/danny_bass_feedback.png';
 import eagleCrestSunset from './images/eagle_crest_sunset.png';
 import eagleCrestSunset2 from './images/eagle_crest_sunset2.png';
 import entrainLunch from './images/entrain_lunch.png';
+import estellaBrotherRollei from './images/estella_brother_rollei.png';
+import fadedGraffiti from './images/faded_graffiti.png';
+import fadedMural from './images/faded_mural.png';
+import greyTrunk from './images/grey_trunk.png';
+import kaycie from './images/kaycie.png';
+import longbeachKid from './images/longbeach_kid.png';
+import momAntonioBeach from './images/mom_antonio_beach.png';
+import nikkiSynth from  './images/nikki_synth.jpg';
+import notnotsEric from './images/notnauts_eric.png';
+import otdHalloween1 from './images/otd_halloween1.jpeg';
+import otdHalloween2 from './images/otd_halloween2.jpeg';
+import parkingLot from './images/parking_lot.png';
+import porterPoint from './images/porter_point.jpg';
+import proposalRock from './images/proposal_rock.png';
+import ripleyGuitar from './images/ripley_guitar.png';
+import schnitzer from './images/schnitzer.jpg';
+import seanCostume from './images/sean_costume.png';
+import seanGuitar from './images/sean_guitar.jpg';
+import swimBuddies from './images/swim_buddies.png';
+import tessaBeach from './images/tessa_beach.jpg';
+import yaquinaHeadLighthouse from './images/yaquina_head_lighthouse.png';
+import zoranPatSwim from './images/zoran_pat_swim.png';
+import zoranPolaroid from './images/zoran_polaroid.jpg';
 
 
 
@@ -78,6 +101,100 @@ const imageData = [
         image: entrainLunch,
         text: 'Alex Tasting Cherry Blossoms',
     },
+    {
+        image: estellaBrotherRollei,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: fadedGraffiti,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: fadedMural,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: greyTrunk,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: kaycie,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: longbeachKid,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: momAntonioBeach,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: nikkiSynth,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: notnotsEric,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: otdHalloween1,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: otdHalloween2,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: parkingLot,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: porterPoint,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: proposalRock,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: ripleyGuitar,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: schnitzer,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: seanCostume,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: seanGuitar,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: swimBuddies,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: tessaBeach,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: yaquinaHeadLighthouse,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: zoranPatSwim,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+    {
+        image: zoranPolaroid,
+        text: 'Alex Tasting Cherry Blossoms',
+    },
+
+
 ]
 
 export default imageData
