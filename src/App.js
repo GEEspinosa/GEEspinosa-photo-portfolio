f0c5846797758b816bd2ac9961e001d9ab782b91
Gabriel Espinosa <gabiespinosa138@gmail.com>
2024-12-16 01:02:00 -0800

    fixed adding eventlister for keydown strokes with useEffect hook to stop react re-render from making multiple listeners.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,74 @@
-import React, { useState } from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Header from './components/common/header/header';
-import Landing from './components/pages/landing';
-import Portfolio from './components/pages/portfolio'
-import About from './components/pages/about';
-import ModalComponent from './components/common/modal/modal';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/common/header/header";
+import Landing from "./components/pages/landing";
+import Portfolio from "./components/pages/portfolio";
+import About from "./components/pages/about";
+import ModalComponent from "./components/common/modal/modal";
+import imageData from "./assets/image-data";
 
 function App() {
-  const [open, setOpen] = useState(false)
-  const [modalSelect, setModalSelect] = useState(0)
-  const [showModal, setShowModal] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [modalSelect, setModalSelect] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
   function imageClickHandler(id) {
-    setModalSelect(id)
-    setShowModal(true)
+    setModalSelect(id);
+    setShowModal(true);
   }
-  
+
   window.addEventListener("resize", () => {
-      if(window.innerWidth > 710) {setOpen(false)}
+    if (window.innerWidth > 710) {
+      setOpen(false);
     }
-  )
+  });
 
-  document.addEventListener('keydown', (e) => {
-    if(e.key === 'Escape') {
-      setShowModal(false)
+  function keyDownHandler(e) {
+    if (e.key === "Escape") {
+      setShowModal(false);
     }
-  })
- 
+    if (e.key === "ArrowLeft" && showModal) {
+      let left = modalSelect - 1;
+      if (left >= 0) {
+        setModalSelect(left);
+      } else {
+        setModalSelect(0);
+      }
+    }
+    if (e.key === "ArrowRight" && showModal) {
+      let right = modalSelect + 1;
+      if (right < imageData.length - 1) {
+        setModalSelect(right);
+      } else {
+        setModalSelect(imageData.length - 1);
+      }
+    }
+  }
+
+  //dev note: because of react re-renders,
+  //eventlisteners must be added and removed with useEffect hook!!!
+  //the eventlistener calls the above declared keyDownHandler
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler);
+    return () => document.removeEventListener("keydown", keyDownHandler);
+  });
+
   return (
-    <>  
-        {showModal &&
-          <ModalComponent 
-          setShowModal={setShowModal}
-          modalSelect={modalSelect}
-          />
-        }
+    <>
+      {showModal && (
+        <ModalComponent setShowModal={setShowModal} modalSelect={modalSelect} />
+      )}
 
-        <Header 
-          open = {open}
-          setOpen = {setOpen}
-          showModal ={showModal}
+      <Header open={open} setOpen={setOpen} showModal={showModal} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Landing imageClickHandler={imageClickHandler} />}
         />
-        <Routes>
-          <Route path="/" element={
-            <Landing 
-              imageClickHandler={imageClickHandler}
-            />
-            }
-          />
-          <Route path="/portfolio" element={<Portfolio />}/>
-          <Route path="/about" element={<About />}/>
-        </Routes>    
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
