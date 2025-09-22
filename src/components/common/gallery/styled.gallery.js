d069ff58423809bcea024de54bd135a381de8196
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-09-21 22:31:38 -0700

    fix: comment out width and height from img elements displayed in gallery styled-component, fixes weird Safari styling issue.

--- src/components/common/gallery/styled.gallery.js
+++ src/components/common/gallery/styled.gallery.js
@@ -26,8 +26,8 @@ const Gallery = styled.div`
   }
 
   img {
-    width: 100%;
-    height: 100%;
+    // width: 100%;
+    // height: 100%;
     max-height: 100%;
     min-width: 100%;
     object-fit: cover;
