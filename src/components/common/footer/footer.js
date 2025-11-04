c1245aa50813bca8c58ee6c486fceaa441917fb5
Gabriel Espinosa <gabiespinosa138@gmail.com>
2025-11-04 14:34:11 -0800

    chore: simple commit to reiterate last that added a footer and adjusted App.js jsx to register on my GitHub contributions to prove work done.

--- src/components/common/footer/footer.js
+++ src/components/common/footer/footer.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const FooterContainer = styled.footer`
   height: 40px;
-  //background: #f7f7f7;
   color: #999;
   display: flex;
   justify-content: center;
@@ -10,8 +9,6 @@ const FooterContainer = styled.footer`
   font-size: 0.9rem;
   user-select: none;
   flex-shrink: 0;
-  /* optional: add a subtle top border */
-  //border-top: 1px solid #ddd;
 `;
 
 function Footer() {
